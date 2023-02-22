3e5d0003556e960a93529b40cf473b432ac382d9
Christ <117885718+ChristBernardS@users.noreply.github.com>
2023-02-22 17:54:14 +0700

    Add files via upload

--- 4_D_71220833.py
+++ 4_D_71220833.py
@@ -1,24 +1,24 @@
 nama_lo = input('Masukkan Nama Lengkap Anda: ')
 prodi_lo = input('Masukkan Prodi Anda: ')
-nilai_lo = input('Masukkan Nilai (dalam Huruf) yang Anda Dapat: ').lower()
+nilai_lo = input('Masukkan Nilai (dalam Huruf) yang Anda Dapat: ')
 try:
-    if nilai_lo == 'a':
-        print('Nilai anda adalah 4.0')
-    elif nilai_lo == 'a-':
+    if nilai_lo == 'A':
+        print('Nilai anda adalah 4.0, tbl tbl serem bgt lohh')
+    elif nilai_lo == 'A-':
         print('Nilai anda adalah 3.75, kamu keren!')
-    elif nilai_lo == 'b+':
+    elif nilai_lo == 'B+':
         print('Nilai anda adalah 3.25')
-    elif nilai_lo == 'b':
+    elif nilai_lo == 'B':
         print('Nilai anda adalah 3.0')
-    elif nilai_lo == 'b-':
+    elif nilai_lo == 'B-':
         print('Nilai anda adalah 2.75, kurang semangat belajar nihh')
-    elif nilai_lo == 'c+':
+    elif nilai_lo == 'C+':
         print('Nilai anda adalah 2.25')
-    elif nilai_lo == 'c':
+    elif nilai_lo == 'C':
         print('Nilai anda adalah 2.0')
-    elif nilai_lo == 'd':
+    elif nilai_lo == 'D':
         print('Nilai anda adalah 1.0, apakah sudah ada pikiran untuk pindah jurusan?')
-    elif nilai_lo == 'e':
+    elif nilai_lo == 'E':
         print('Nilai anda adalah 0, niat kuliah nggak sih???')
 except:
     print('Inputan nilai yang anda masukkan tidak valid')
